414e63ab2f6a5f317d7992a62b1f7886ba77cea3
Mak7991 <alikhanmaaz1997@gmail.com>
2023-08-21 10:30:49 +0500

    done with movie listing and store function

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { TodosComponent } from './components/todos/todos.component';
 import { MovieListComponent } from './components/movie-list/movie-list.component';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ShippingComponent } from './components/shipping/shipping.component';
 
 const routes: Routes = [
   { path: '', component: TodosComponent },
   { path: 'movies', component: MovieListComponent },
   { path: 'movies/:movie_id', component: MovieDetailsComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'shipping', component: ShippingComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './components/todos/todos.component';
 import { TodoItemsComponent } from './components/todo-items/todo-items.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { AddTodoItemsComponent } from './components/add-todo-items/add-todo-items.component';
 import { MovieListComponent } from './components/movie-list/movie-list.component';
 import { MovieShareAlertComponent } from './components/movie-share-alert/movie-share-alert.component';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ShippingComponent } from './components/shipping/shipping.component';
 
 @NgModule({
   declarations: [
@@ -19,12 +22,16 @@ import { MovieDetailsComponent } from './components/movie-details/movie-details.
     AddTodoItemsComponent,
     MovieListComponent,
     MovieShareAlertComponent,
-    MovieDetailsComponent
+    MovieDetailsComponent,
+    CartComponent,
+    ShippingComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
